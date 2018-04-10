276323e369627f9f962030917314b61af52ac922
TanTan <tantanwang@gmail.com>
2018-04-10 03:06:50 -0400

    moved around js

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const app = express()
 // process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public', 'index.html'));
+
 
 app.listen(port, () => {
     console.log(`Now running on port ${port}`);
