1080ca50947c17540e446910a24d477dad18647b
TanTan <tantanwang@gmail.com>
2018-04-10 04:03:03 -0400

    res.sendfile

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const app = express()
 // process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8080;
 
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res){
 	var path = require('path');
-  	res.sendFile(path.join(__dirname+'/index.html'));
+  	res.sendFile(path.join(__dirname+'/public/index.html'));
  }); 
 
 app.listen(port, () => {
