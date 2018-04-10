ae9069a911b1dff5d4769679a48eb4e6cfe4e30e
TanTan <tantanwang@gmail.com>
2018-04-10 03:08:59 -0400

    moved around js

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ const app = express()
 var port = process.env.PORT || 8080;
 
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public', 'index.html'));
-
+app.get('/', function(req, res){
+     res.sendFile(path.join(__dirname+'/public/index.html'));
+ });
 
 app.listen(port, () => {
     console.log(`Now running on port ${port}`);
