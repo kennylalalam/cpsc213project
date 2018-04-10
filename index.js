d1f591ceda19923be44c65fce9139355ecb5830d
TanTan <tantanwang@gmail.com>
2018-04-10 03:23:21 -0400

    restored hello world

--- index.js
+++ index.js
@@ -4,16 +4,10 @@ const app = express()
 // process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8080;
 
-server.use('/public', express.static(__dirname + '/public'));
 
-server.get('/*', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
-
-/*
 app.get('/', function(req, res){
-     res.sendFile(path.join(__dirname+'/public/index.html'));
- }); */
+    res.send("hello world!");
+ }); 
 
 app.listen(port, () => {
     console.log(`Now running on port ${port}`);
