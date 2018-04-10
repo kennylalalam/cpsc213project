f10db34f8e5f8ffdc345bf273b4e34d85fea6d2f
TanTan <tantanwang@gmail.com>
2018-04-10 04:07:13 -0400

    res.sendfile

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 
+app.use(express.static('public'))
 // process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8080;
 
